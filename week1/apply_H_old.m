2b04c71b65bfc370324412c7beb320c442bd86f2
Guillem Cucurull <gcucurull@gmail.com>
2017-03-07 11:23:57 +0100

    New apply H

--- week1/apply_H_old.m
+++ week1/apply_H_old.m
@@ -1,19 +1,11 @@
-function [ out_im ] = apply_H( im, T)
+function [ out_im ] = apply_H_old( im, T)
 % Receives an image 'im' and a matrix transofrmation 'T'
     
 %%% 1. %%% compute output image size
-    %T = inv(T);
     corners = [1,1,size(im,1),size(im,1);
                 1, size(im,2), 1, size(im,2);
                 1, 1, 1, 1];
-            
-    t_corners = T*corners;
-    % normalize
-    t_corners(:,1) = t_corners(:,1) / t_corners(3,1);
-    t_corners(:,2) = t_corners(:,2) / t_corners(3,2);
-    t_corners(:,3) = t_corners(:,3) / t_corners(3,3);
-    t_corners(:,4) = t_corners(:,4) / t_corners(3,4);
-    t_corners = round(t_corners);
+    t_corners = round(T*corners);
     
     % compute distances between transformer corner points
     i_dist = abs(min(t_corners(1,:))-max(t_corners(1,:))); % first row are i coordinates
@@ -24,26 +16,28 @@ function [ out_im ] = apply_H( im, T)
     j_disp = min(t_corners(2,:));
             
     out_im=zeros(i_dist, j_dist, 3);
-    %T = inv(T);
+    T = inv(T);
+    
+    from = im;
+    to = out_im;
         
 %%% 2. %%% Compute homography
-    for i=1:size(im,1)
-        for j=1:size(im,2)
-            v1 = [i;j;1]; % point in current image
+    for i=i_disp:size(out_im,1)
+        for j=j_disp:size(out_im,2)
+            v1 = [i;j;1]; % point in target image
             v2 = T*v1;
             v3 =v2/v2(3,1);
-            v3 = round(v3); % point in target image
-            if (v3(1,1)-i_disp+1<=0 || v3(2,1)-j_disp+1<=0 || v3(1,1)-i_disp+1 > size(out_im,1) || v3(2,1)-j_disp+1 > size(out_im,2))
+            v3 = round(v3); % point in source image
+            if (v3(1,1)<=0 || v3(2,1)<=0 || v3(1,1) > size(im,1) || v3(2,1) > size(im,2))
                continue;
             else
                 % copy from source to target
-                %out_im(i-i_disp+1,j-j_disp+1,:) = im(v3(1,1),v3(2,1),:);
-                out_im(v3(1,1)-i_disp+1,v3(2,1)-j_disp+1,:) = im(i,j,:);
+                to(i-i_disp+1,j-j_disp+1,:) = from(v3(1,1),v3(2,1),:);
             end
         end
     end
     
-    %out_im = to;
+    out_im = to;
 
 end
 
